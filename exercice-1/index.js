921221cee6c73b1fd58a5657d38fe76e59eeb4c6
Taurouf <tauroufouf@gmail.com>
2020-09-25 15:29:04 +0200

    feat(grid generator): create grid generator" "
    "
    "
    "
    "
    "
    "

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -1,1 +1,36 @@
 'use strict'
+
+var GridGenerator = function gridGenerator(xAxis, yAxis) {
+  this.xAxis = xAxis;
+  this.yAxis = yAxis;
+
+}
+
+GridGenerator.prototype.run = function () {
+  return this.grid();
+}
+
+
+
+
+GridGenerator.prototype.grid = function () {
+  var matrix = document.createElement("table");
+  for (var i = 0; i < this.xAxis; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 0; j < this.yAxis; j++) {
+      var td = document.createElement("td");
+      td.style.border = 'solid';
+      td.style.width = '20px';
+      td.style.height = '20px';
+      tr.appendChild(td);
+    }
+    matrix.appendChild(tr);
+  }
+  document.querySelector("body").appendChild(matrix);
+
+
+}
+
+var toto = new GridGenerator(5, 5);
+toto.run();
+
