1fd2cc9ea3d7b412de6507e2cfb5ef15275375aa
Taurouf <tauroufouf@gmail.com>
2020-10-13 23:02:19 +0200

    feat(form): create function create form

--- exercice-6/index.js
+++ exercice-6/index.js
@@ -1,1 +1,29 @@
-'use strict'
+'use strict';
+
+var MyNumberCheck = function MyNumberCheck() {
+
+}
+
+MyNumberCheck.prototype.run = function () {
+  this.form();
+}
+
+MyNumberCheck.prototype.form = function () {
+  var form = document.createElement('form');
+  var tel = document.createElement('input');
+  tel.className = 'tel';
+  tel.setAttribute('placeholder', 'Numero');
+  tel.setAttribute('type', 'tel');
+  form.appendChild(tel);
+  var submit = document.createElement('input');
+  submit.className = 'button'
+  submit.value = 'Confirmer';
+  submit.setAttribute('type', 'button');
+  form.appendChild(submit);
+  form.addEventListener("keydown", function (e) { if (e.keyCode == 13) e.preventDefault() });
+  document.body.appendChild(form);
+}
+
+
+var number1 = new MyNumberCheck();
+number1.run();
