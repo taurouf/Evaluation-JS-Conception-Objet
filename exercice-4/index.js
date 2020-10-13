3d3fc598ffa0f357778cb1e12cb93b8928a679f9
Taurouf <tauroufouf@gmail.com>
2020-10-13 21:34:50 +0200

    feat(selectedCountry): create function for selected country

--- exercice-4/index.js
+++ exercice-4/index.js
@@ -1,1 +1,37 @@
 'use strict'
+
+
+
+var RenderWorldMap = function RenderWorldMap() {
+
+}
+RenderWorldMap.prototype.run = function() {
+    this.select();
+    this.legend();
+  }
+
+RenderWorldMap.prototype.select = function() {
+    var country = document.body.querySelectorAll('path');
+    country.forEach(function(pays) {
+      pays.addEventListener('click', function(e){
+        country.forEach(function(path) {
+          path.style.fill = 'black';
+        })
+        e.target.style.fill = 'red';
+      });
+      pays.addEventListener('mouseenter', function(e) {
+        if(e.target.style.fill === 'red') {
+          e.target.style.fill = 'purple';
+     
+        }
+        e.target.style.fill = 'blue';
+      })
+      pays.addEventListener('mouseleave', function(e) {
+        if(e.target.style.fill === 'purple' || e.target.style.fill == 'red') {
+          e.target.style.fill = 'red';
+          return;
+        }
+        e.target.style.fill = 'black';
+      })
+    })}
+
