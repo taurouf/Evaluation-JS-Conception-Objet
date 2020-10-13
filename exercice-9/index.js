f3ebb2ef394c1d8964c12a8ecff0a396a1afd5b8
Taurouf <tauroufouf@gmail.com>
2020-10-14 00:02:03 +0200

    feat(palindrome): create a function palindrome

--- exercice-9/index.js
+++ exercice-9/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+var str = prompt('Entrer votre mots ou phrases pour voir si c\'est un palindrome');
+var isPalindrome = function (inStr) {
+  inStr = inStr.replace(/\W/g, '');
+  inStr = inStr.toLowerCase();
+  for (var i = 0; i < inStr.length; i++) {
+    if (inStr[i] !== inStr[inStr.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPalindrome(str));
