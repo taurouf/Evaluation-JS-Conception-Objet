dbdc1ecba1fac8a99f9ee55d0f7389bdaaf7a893
Taurouf <tauroufouf@gmail.com>
2020-10-14 23:08:18 +0200

    style(formatting): formated code 2 spaces

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -10,29 +10,28 @@ var MyAverage = function MyAverage() {
 /**
  * Run MyAverage
  */
-MyAverage.prototype.run = function() {
+MyAverage.prototype.run = function () {
   this.compute();
 }
 /**
  * Function to calculate a grade average
  */
-MyAverage.prototype.compute = function() {
-    var i=0;
-    var somme=0;
-    var moyenne=0;
-    var nb_notes=0;
-    nb_notes=prompt("Vous souhaitez faire la moyenne de combien de notes ?","Nb de Notes");
-    nb_notes=parseInt(nb_notes);
-    var notes = new Array(nb_notes);
-    for(i=1;i<=nb_notes;i++)
-    {
-         
-        var note_i=prompt("Entrez la note");
-        notes[i-1]=parseInt(note_i);
-        somme+=notes[i-1];
-    }
-    moyenne=(somme/nb_notes);
-    alert("La moyenne des notes est : "+moyenne);
+MyAverage.prototype.compute = function () {
+  var i = 0;
+  var somme = 0;
+  var moyenne = 0;
+  var nb_notes = 0;
+  nb_notes = prompt("Vous souhaitez faire la moyenne de combien de notes ?", "Nb de Notes");
+  nb_notes = parseInt(nb_notes);
+  var notes = new Array(nb_notes);
+  for (i = 1; i <= nb_notes; i++) {
+
+    var note_i = prompt("Entrez la note");
+    notes[i - 1] = parseInt(note_i);
+    somme += notes[i - 1];
+  }
+  moyenne = (somme / nb_notes);
+  alert("La moyenne des notes est : " + moyenne);
 }
 
 var average = new MyAverage();
